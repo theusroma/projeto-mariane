25c314662b6e8074b6ec62cfeb8ac6576fe148ff
João César <164185005+j0a0cesar@users.noreply.github.com>
2024-10-31 18:35:01 -0300

    Create agradecimentos.c

---
+++ animacoes/agradecimentos.c
@@ -0,0 +1,49 @@
+// Bibliotecas Padrão
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <windows.h>
+
+// Protótipo
+void agradecimentos();
+
+	void agradecimentos(){//tá com erro no obrigado
+    
+    printf("\t\t  #####                      ##										 \n"); Sleep(40);
+    printf("\t\t ##   ##   ######  ######    ##  #######   #####     #####    ######  \n"); Sleep(40);
+    printf("\t\t ##	  ##   ##   ## ##   ##   ##  ##   	  ##   ##    ##  ##  ##    ## \n"); Sleep(40);
+    printf("\t\t ##   ##   ######  #######   ##  ##  ###  #######    ##   ## ##    ## \n"); Sleep(40);
+    printf("\t\t ##   ##   ##   ## ##  ##    ##  ##   ##  ##   ##    ##  ##  ##    ## \n"); Sleep(40);
+    printf("\t\t  #####    ######  ##   ##   ##   ######  ##   ##    #####    ######  \n\n"); Sleep(40);
+    
+    	    printf("\t\t #######                          \n"); Sleep(40);
+    		printf("\t\t ##   ##                          \n"); Sleep(40);
+    		printf("\t\t ##   ##  ######  ##       #####  \n"); Sleep(40);
+    		printf("\t\t ######   ##      ##      ##   ## \n"); Sleep(40);
+    		printf("\t\t ##       ####    ##      ####### \n"); Sleep(40);
+    		printf("\t\t ##       ##      ##      ##   ## \n"); Sleep(40);
+    		printf("\t\t ##       ######  ######  ##   ## \n\n"); Sleep(40);
+    		
+    		printf("\t\t ####### ##   ##   ##### \n"); Sleep(40);
+    		printf("\t\t ##      ##   ##  ##   ##\n"); Sleep(40);
+    		printf("\t\t ######  ##   ##  #######\n"); Sleep(40);
+    	    printf("\t\t     ##  ##   ##  ##   ##\n"); Sleep(40);
+    	    printf("\t\t#######  #######  ##   ##\n\n"); Sleep(40);
+    
+    printf("\t\t ######                                         	       			\n"); Sleep(40);
+    printf("\t\t##    ##                        	                        		\n"); Sleep(40);
+    printf("\t\t##    ##  ########  ######  ###    ##   #####   #####    #####   ## \n"); Sleep(40);
+    printf("\t\t########     ##     ##      ## ##  ##  ##      ##   ##  ##   ##  ## \n"); Sleep(40);
+    printf("\t\t##    ##     ##     ####    ##  ## ##  ##      #######  ##   ##  ## \n"); Sleep(40);
+    printf("\t\t##    ##     ##     ##      ##   ####  ##      ##   ##  ##   ##     \n"); Sleep(40);
+    printf("\t\t##    ##     ##     ######  ##     ##   #####  ##   ##   #####   ## \n"); Sleep(40);
+   	printf("\t\t                                          #                    		\n\n"); Sleep(40);
+    						
+    						
+}
+
+int main(){
+	system("color 0E");
+	agradecimentos();
+return 0;	
+}

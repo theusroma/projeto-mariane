c4f8efffd8bf0e5975d2ab71754ff61718e53a01
João César <164185005+j0a0cesar@users.noreply.github.com>
2024-10-31 16:51:16 -0300

    Update nome.c

--- animacoes/nome.c
+++ animacoes/nome.c
@@ -1,6 +1,9 @@
-
+// Bibliotecas Padrão
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
+// Protótipo
 void nome();
 
 	void nome() {
@@ -37,6 +40,7 @@ void nome();
 }
 
 int main(){
+	system("color 0E");
 	nome();
 return 0;	
 }

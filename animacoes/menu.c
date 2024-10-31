836fd595533a40d4661040afd3d87a6f79a28d75
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2024-10-30 21:08:23 -0300

    Update menu.c

--- animacoes/menu.c
+++ animacoes/menu.c
@@ -9,9 +9,14 @@ void arte_menu();
 
 
 void arte_menu(){
-	printf("\n\n");
-	printf("\t		Bem Vindo(a) ao Jardim das Abelhas!\n");
-	printf("\t                    _\n");	
+    printf("\n\n");
+    printf("\t		Bem Vindo(a) ao Jardim das Abelhas!\n");
+    printf("\t		      	  	       __\n");
+    printf("\t      		       .' '.          // \\  \n");
+    printf("\t            .          .   .          \\\\_/ //\n");
+    printf("\t               .         .         . -(||)(')\n");
+    printf("\t                 ' .  . ' ' .  . '    '''\n");
+    printf("\t                    _\n");	
     printf("\t                  _(_)_                          wWWWw    _\n");
     printf("\t      @@@@       (_)@(_)   vVVVv     _     @@@@  (___)  _(_)_\n");
     printf("\t     @@()@@ wWWWw  (_)\\    (___)   _(_)_  @@()@@   Y   (_)@(_)\n");

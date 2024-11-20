bc86ef83ab918c2b7eb0d204623dc9eb11360ad0
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2024-11-20 20:16:09 -0300

    Update venda.h

--- brubs/venda.h
+++ brubs/venda.h
@@ -8,6 +8,7 @@
 void arte_menu();
 void arte_venda();
 void menu_venda(char **flores, int *quantidade);
+void casoZero(char **flores, int *quantidade);
 int vendaFlores(char **flores, int *quantidade);
 
 void arte_venda(){
@@ -30,7 +31,7 @@ void arte_venda(){
 }
 
 void menu_venda(char **flores, int *quantidade){
-	int i, opcaoV, sair = 0, total = 0;
+	int i, opcaoV;
 	
 	system("cls");
 	arte_venda();
@@ -80,33 +81,7 @@ void menu_venda(char **flores, int *quantidade){
                 //break;
                 return;
             case 0: //finalizar pedido
-            system("cls");
-            arte_venda();
-	            printf("\n");
-				printf("\t  ____________________________________________________________ \n");
-				printf("\t |                                                            | \n");
-				printf("\t |              Venda finalizada! Resumo do pedido:           | \n");
-				printf("\t |------------------------------------------------------------| \n");
-				
-				
-				
-			    printf("\t |============================================================| \n");
-			    
-			    for (i = 0; i < NUM_FLORES; i++) {
-			        if (quantidade[i] > 0) { 
-			            printf("\t- %s: %d unidades\n", flores[i], quantidade[i]);
-			            total += quantidade[i]; // soma o total de flores compradas
-			        }
-			    }
-			    if (total == 0) {
-			        printf("\tNenhuma flor foi comprada.\n");
-			    } else {
-			        printf("\n\tTotal de flores compradas: %d\n", total);
-			    }
-			    printf("\t===================================\n");
-			    printf("\n\tObrigado pela compra!\n");
-			    Sleep(8000);
-			    //sair = 1;
+            	casoZero(flores, quantidade);
             	break;
             default:
             	arte_menu();
@@ -116,27 +91,16 @@ void menu_venda(char **flores, int *quantidade){
 
     } while(opcaoV != 0); //loop até que o usuário escolha "voltar" ou "finalizar"
     
-    
-    
-    
 }
-	/*
-	printf("\t |                                                         |");
-    printf("\n\t |       ###########  FORMA DE PAGAMENTO ###########       |");
-    printf("\n\t |                                                         |");
-    printf("\n\t | [1] - Cartão de débito                                  |");
-    printf("\n\t | [2] - Cartão de crédio                                  |");
-    printf("\n\t | [3] - Vale refeição                                     |");
-    printf("\n\t | [4] - Dinheiro                                          |"); */
 
 int vendaFlores(char **flores, int *quantidade){
 	int i, op_flores, quant_flor;
-	const char *nomes[NUM_FLORES] = {"Lírios", "Rosas", "Girassóis", "Violetas", "Orquídeas", "Astromélias", "Tulipas"};
+	//const char *nomes[NUM_FLORES] = {"Lírios", "Rosas", "Girassóis", "Violetas", "Orquídeas", "Astromélias", "Tulipas"};
 	
 	//Preenchendo o vetor nome com o nome das flores
-	for (i = 0; i < NUM_FLORES; i++) {
-        	strcpy(flores[i], nomes[i]); 
-    	}
+	//for (i = 0; i < NUM_FLORES; i++) {
+    //    	strcpy(flores[i], nomes[i]); 
+    //	}
     //fazer um vetor que armazene só as flores escolhidas, e adicionar os valores, talvez struct
 	printf("\t |            		                                      | \n ");
 	printf("\t |            		                                      | \n ");
@@ -161,7 +125,7 @@ int vendaFlores(char **flores, int *quantidade){
                 break; // Finaliza o pedido
             	}
 
-            if (op_flores < 1 || op_flores > 7) {
+            if (op_flores < 1 || op_flores > NUM_FLORES) {
             	printf("\t | Opção inválida! Pressione qualquer tecla.                  |");
 				printf("\n\t |            		                                      | \n ");
                 scanf("%d", &op_flores);	
@@ -171,7 +135,47 @@ int vendaFlores(char **flores, int *quantidade){
 				quantidade[op_flores - 1] += quant_flor; //atualiza a quantidade na struct
             }
             
-	} while (op_flores != 0); 
-		
-	return 0;
+	} while (op_flores != 0); 	
+}
+
+void casoZero(char **flores, int *quantidade){
+	int i, total = 0, pagamento;
+    	
+	system("cls");
+	arte_venda();
+	
+	printf("\t |            		                                      | \n ");
+	printf("\t |         =========== FORMA DE PAGAMENTO ===========         | \n");
+	printf("\t |------------------------------------------------------------| \n");
+	printf("\t |  [1] Cartão de débito                                      | \n");
+	printf("\t |  [2] Cartão de crédito                                     | \n");
+	printf("\t |  [3] Dinheiro                                              | \n");
+	printf("\t |  [4] PIX	                                              | \n");
+	printf("\t | Opção de pagamento: ");
+	scanf("%d", &pagamento);
+
+	if (pagamento >= 1 && pagamento <= 4){
+		printf("\t |============================================================| \n");
+		printf("\t |            		                                      | \n ");
+		printf("\t |              Venda finalizada! Resumo do pedido:           | \n");
+		printf("\t |------------------------------------------------------------| \n");
+	
+	
+	    for (i = 0; i < NUM_FLORES; i++) {
+	        if (quantidade[i] > 0) { 
+	            printf("\t | - %s: %d unidades\n", flores[i], quantidade[i]);
+	            total += quantidade[i]; // soma o total de flores compradas
+	        }
+	    }
+	    if (total == 0) {
+	        printf("\tNenhuma flor foi comprada.\n");
+	    } else {
+	        printf("\n\tTotal de flores compradas: %d\n", total);
+	    }
+	    printf("\t===================================\n");
+	    printf("\n\tObrigado pela compra!\n");
+	    Sleep(8000);
+	    //sair = 1;	
+	}
+	
 }

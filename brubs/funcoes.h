e160c74113e1b95b4ac879763c1eaea0d3ec3672
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2024-11-21 21:58:51 -0300

    Create funcoes.h

---
+++ brubs/funcoes.h
@@ -0,0 +1,27 @@
+#define NUM_FLORES 7
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <windows.h>
+#include <conio.h>//biblioteca para usar getch()
+
+void cadastro();
+void remover();
+void consulta();
+void lista();
+
+void cadastro(){
+	arte_funcoes();
+}
+
+void remover(){
+	arte_funcoes();
+}
+
+void consulta(){
+	arte_funcoes();
+}
+
+void lista(){
+	arte_funcoes();
+}

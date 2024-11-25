69a4574240170934302cda0be3e1b4343deb055a
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2024-11-25 16:25:30 -0300

    Update animacao.h

--- brubs/animacao.h
+++ brubs/animacao.h
@@ -67,7 +67,7 @@ void arte_menu(){
 	printf("\t            .          .   .          \\\\_/ //\n");
 	printf("\t               .         .         . -(||)(')\n");
 	printf("\t                 ' .  . ' ' .  . '    '''\n");
-	printf("\t                   _\n");	
+	printf("\t                    _\n");	
     printf("\t                  _(_)_                          wWWWw    _\n");
     printf("\t      @@@@       (_)@(_)   vVVVv     _     @@@@  (___)  _(_)_\n");
     printf("\t     @@()@@ wWWWw  (_)\\    (___)   _(_)_  @@()@@   Y   (_)@(_)\n");
